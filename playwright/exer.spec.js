85d4dedefef57292da5594c532cc3631a22a3e17
th32768p4 <th32768@outlook.com>
2025-02-24 20:50:39 +0800

    add new test Changes to be committed: 	modified:   playwright/exer.spec.js

--- playwright/exer.spec.js
+++ playwright/exer.spec.js
@@ -6,4 +6,11 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
+  test('a known page can be opened',async({ page }) => {
+    await page.goto('')
+    await expect(page.getByText('ivysaur')).toBeVisible()
+
+    await page.goto('/pokemon/ivysaur')
+    await expect(page.getByText('special attack')).toBeVisible()
+  })
 })
