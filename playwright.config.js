830589907bc68bb9489c9add16eca681fa887b94
th32768p4 <th32768@outlook.com>
2025-02-24 20:26:50 +0800

    Fix workflow config Changes to be committed: 	modified:   package.json 	modified:   playwright.config.js 	deleted:    playwright/example.spec.js 	modified:   playwright/exer.spec.js 	deleted:    tests-examples/demo-todo-app.spec.js

--- playwright.config.js
+++ playwright.config.js
@@ -31,6 +31,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    baseURL: 'http://localhost:8080/',
   },
 
   /* Configure projects for major browsers */
@@ -70,7 +71,12 @@ export default defineConfig({
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
-
+  webServer: {
+    command: 'npm run start',
+    url: 'http://127.0.0.1:8080',
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
   /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
--- playwright/exer.spec.js
+++ playwright/exer.spec.js
@@ -2,7 +2,7 @@ const { test, describe, expect } = require('@playwright/test')
 
 describe('Pokedex', () => {
   test('front page can be opened', async ({ page }) => {
-    await page.goto('http://localhost:8080/')
+    await page.goto('')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
